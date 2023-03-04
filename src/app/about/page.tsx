4341a1546cd9d2a8a5b6492b5817b3b88408094a
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2023-03-04 17:40:47 -0300

    feat: add routes

---
+++ src/app/about/page.tsx
@@ -0,0 +1,34 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+        pariatur ipsa officiis voluptate rem, assumenda illo fugiat ipsum,
+        repellendus ducimus quos esse consequatur! Non ab rerum voluptatibus
+        esse ratione dolorum! Non, voluptatem vel. Nisi vero optio a dignissimos
+        repudiandae. Iusto eveniet ex quas aut vel quibusdam asperiores non
+        dolorem quisquam cum ab saepe repudiandae, repellendus facilis tenetur
+        expedita unde omnis? In, cum error aperiam libero aliquid reprehenderit.
+        Quae sint rerum deserunt nesciunt cum eligendi ipsum quas fugiat,
+        perspiciatis id mollitia quibusdam. Ad doloremque quaerat temporibus
+        sequi officia culpa nostrum ex?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+        pariatur ipsa officiis voluptate rem, assumenda illo fugiat ipsum,
+        repellendus ducimus quos esse consequatur! Non ab rerum voluptatibus
+        esse ratione dolorum! Non, voluptatem vel. Nisi vero optio a dignissimos
+        repudiandae. Iusto eveniet ex quas aut vel quibusdam asperiores non
+        dolorem quisquam cum ab saepe repudiandae, repellendus facilis tenetur
+        expedita unde omnis? In, cum error aperiam libero aliquid reprehenderit.
+        Quae sint rerum deserunt nesciunt cum eligendi ipsum quas fugiat,
+        perspiciatis id mollitia quibusdam. Ad doloremque quaerat temporibus
+        sequi officia culpa nostrum ex?
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
